d2547c2ffca3c5dae5c739b0ffb076b26a434f00
Kevin Santos Da Silva <kevinssilva1502@gmail.com>
2024-05-11 23:37:23 -0300

    protipo tcc

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import { useState } from 'react'
+
+import { FaLock, FaUser } from 'react-icons/fa'
+
+import "./Login.css";
+
+const Login = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Envio"); 
+  };
+
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Faça o Login < br/> KitFit</h1>
+ 
+        <div className="input-field">
+        <input type="email" placeholder="E-mail"
+        onChange={(e) => setUsername(e.target.value)} />
+        <FaUser className="icon" />
+        </div>
+
+        <div className="input-field">
+          <input type="password" placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)} />
+          <FaLock className="icon" />
+        </div>
+
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu a senha?</a>
+        </div>
+
+        <button>Entrar</button>
+
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registrar</a>
+          </p>
+        </div>
+       </form>
+    </div>
+  )
+} 
+
+export default Login
